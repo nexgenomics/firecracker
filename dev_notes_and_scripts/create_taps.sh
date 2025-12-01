e1ad4b7638bc87705e0445f684385e7fb7b6dbf6
Francis Cianfrocca <garbagecat10@gmail.com>
2025-12-01 14:53:59 +0000

    dev

---
+++ dev_notes_and_scripts/create_taps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# /usr/local/bin/create-firecracker-taps.sh
+for try in {1..30}; do
+    [[ -d /sys/class/net/fire0 ]] && break
+    echo "Waiting for fire0 to appear ($try/30)..."
+    sleep 1
+done
+
+[[ ! -d /sys/class/net/fire0 ]] && { echo "fire0 never appeared"; exit 1; }
+
+for i in {100..199}; do
+    ip tuntap add dev tap$i mode tap user francis 2>/dev/null || break
+    ip link set tap$i master fire0 up 2>/dev/null || sleep 0.1
+done
+
+sleep 1
+systemctl start nftables.service
