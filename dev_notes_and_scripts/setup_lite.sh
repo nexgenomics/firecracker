ab44a7eba6f4dca8e07fbeebb6ea303e7b0fd5d1
Francis Cianfrocca <garbagecat10@gmail.com>
2025-12-01 14:52:51 +0000

    dev

--- dev_notes_and_scripts/setup_lite.sh
+++ dev_notes_and_scripts/setup_lite.sh
@@ -3,8 +3,9 @@
 # We can't set -e because the post-installation of isc-dhcp-client pukes
 # due to no systemd running. The install itself works though.
 # 🤪
+# Aaaand, we're not using that package after all.
 
-#set -e
+set -e
 
 echo "tzdata tzdata/Areas select Etc" | debconf-set-selections
 echo "tzdata tzdata/Zones/Etc select UTC" | debconf-set-selections
@@ -14,12 +15,16 @@ DEBIAN_FRONTEND=noninteractive apt install -y systemd systemd-sysv dbus udev
 apt install -y iproute2 iputils-ping telnet netcat-openbsd vim
 apt install -y cloud-init cloud-initramfs-growroot
 
-echo "isc-dhcp-client isc-dhcp-client/run-dhclient boolean false" | debconf-set-selections
-#ln -sf /bin/true /usr/sbin/invoke-rc.d
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends isc-dhcp-client
+
+# Don't use dhcp in the guest.
+#echo "isc-dhcp-client isc-dhcp-client/run-dhclient boolean false" | debconf-set-selections
+#DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends isc-dhcp-client
+
 rm -fr /var/lib/apt/lists/*
 
+### BOTH OF THESE SHOULD BE DELETED!!!
 echo "root:root" | chpasswd
+echo "ubuntu:ubuntu" | chpasswd
 
 #mv /tmp/fcnet.service /etc/systemd/system
 #mv /tmp/fcnet-setup.sh /usr/local/bin
@@ -33,18 +38,40 @@ echo "root:root" | chpasswd
 #ln -s /etc/systemd/system/fcnet.service /etc/systemd/system/multi-user.target.wants/fcnet.service
 
 #echo "nameserver 8.8.8.8" > /etc/resolv.conf
-#echo "127.0.0.1 localhost" > /etc/hosts
+echo "127.0.0.1 localhost" > /etc/hosts
 
 
 SCRIPT_PATH="/usr/local/bin/setupnetwork.sh"
 SERVICE_NAME="setupnetwork.service"
 
 # --- 1. Create the startup script ---
+#cat << 'EOF' | tee "$SCRIPT_PATH" > /dev/null
+##!/bin/bash
+#echo "Running startup script..."
+#ip link set dev eth0 up
+#dhclient eth0
+#EOF
+
+# shell MUST be bash because of the substitutions used
 cat << 'EOF' | tee "$SCRIPT_PATH" > /dev/null
-#!/bin/sh
-echo "Running startup script..."
-ip link set dev eth0 up
-dhclient eth0
+#!/bin/bash
+INTERFACE="eth0"
+MAC=$(ip link show "$INTERFACE" | awk '/ether/ {print $2}' | tr -d ':')
+if [ -z "$MAC" ] || [ ${#MAC} -ne 12 ]; then
+    echo "Error: Could not read a valid MAC address from $INTERFACE"
+    exit 1
+fi
+LAST4="${MAC: -8}"
+A=$((16#${LAST4:0:2}))
+B=$((16#${LAST4:2:2}))
+C=$((16#${LAST4:4:2}))
+D=$((16#${LAST4:6:2}))
+IP="$A.$B.$C.$D"
+echo "MAC $MAC  →  last 4 octets $LAST4  →  IP $IP"
+ip addr flush dev "$INTERFACE" 2>/dev/null
+ip addr add "$IP/16" dev "$INTERFACE"
+ip link set "$INTERFACE" up
+ip ro add default via 10.0.0.1
 EOF
 
 chmod +x "$SCRIPT_PATH"
